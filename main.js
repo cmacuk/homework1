19f3cc1f866ea1b76bda16b836a2a549f1e55753
Stanislav <cmacuk1-1@mail.ru>
2021-01-17 01:35:13 +0200

    function

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+
+function start() {
+	money = +prompt("Ваш бюджет на месяц?", "");
 	time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+	while (isNaN(money) || money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", "");
+	}
+
+}
+start();
+
 
 let appData = {
 	budget: money,
@@ -10,27 +21,24 @@ let appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
+	savings: true
 };
 
 
-for (let i = 0; i < 2; i++) {
-	let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-		b = prompt("Во сколько обойдется?", "");
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+			b = prompt("Во сколько обойдется?", "");
 
-	if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+		if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+			appData.expenses[a] = b;
+		} else {
+			i--;
+		}
 
-		console.log("done");
-
-		appData.expenses[a] = b;
-	} else {
-		console.log("bad result");
-		i--;
-	}
-
-};
-
-
+	};
+}
+chooseExpenses();
 
 // Используем цикл WHILE
 
